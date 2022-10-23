c5859f37564f5c68eb3684a509b2113e8513a9ee
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-10-23 01:15:08 -0400

    implemented generics for the interfaces and struct.

--- main.go
+++ main.go
@@ -1,22 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-
-// interface has no true implementation 
-	// it only has a function sig
-type Storer interface {
-	Put(string, []byte) error
-	Get(string) ([]byte, error)
-	Update(string, []byte ) error
-	Delete(string, []byte, error)
+// interface has no true implementation
+// it only has a function sig
+type Storer[k comparable, V any] interface {
+	Put(k, V) error
+	Get(k) ([]byte, error)
+	Update(k, V ) error
+	Delete(k)  (V, error)
 }
-type KVStore struct {
-	
+type KVStore [k comparable, V any]struct {
+	// protects the variable of mu
+	mu sync.RWMutex
+	data map[string][]byte
 }
 
-func StoreThings(s Storer) error {
-	return  s.Put("fooo", []byte("barrr"))
+func NewKVStore() *KVStore[string, int] {
+	return &KVStore[string, int]{
+		data: make(map[string][]byte),
+	}
+}
+
+func StoreThings(s Storer[string, int])error {
+	return  s.Put("fooo", 1)
 }
 
 func main(){
